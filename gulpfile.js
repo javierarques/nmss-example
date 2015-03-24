22af138ab6cd4d9cbfe9b50d6e47600002dd44cb
Javier Arques <javiarchena@gmail.com>
2015-03-24 17:56:29 +0100

    add autoprefixer to build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
-var gulp = require('gulp');
-var gls  = require('gulp-live-server');
-var sass = require('gulp-sass');
+var gulp 		 = require('gulp');
+var gls  		 = require('gulp-live-server');
+var sass 	     = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 // Sass compiling
 // Load the npm source and the theme
 gulp.task('sass', function () {
-	gulp.src('scss/app.scss')
-	  .pipe(sass({
-	    includePaths: [
-	    	'node_modules/nmss-theme-sportmaniacs/theme',
-	    	'node_modules/nmss/src',
-    	]
-	  }))
-	  .pipe(gulp.dest('dist/css'));
+	gulp.src('src/scss/app.scss')
+	.pipe(sass({
+        outputStyle: 'compressed',
+        errLogToConsole: true,
+		includePaths: [
+			'node_modules/nmss-theme-prototype/theme',
+			'node_modules/nmss/src',
+		]
+	}))
+	.pipe(autoprefixer('last 2 version'))
+	.pipe(gulp.dest('dist/css'));
 });
 
 
